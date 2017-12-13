1a7a4fa9ce2f9a4be867d46e274e9102c57c947b
Valentina <valentina.rmo@gmail.com>
2017-12-13 15:26:03 +0000

    Add heroku setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ require 'dm-postgres-adapter'
 require_relative '../models/db_helper.rb'
 
 # DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"  )
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"  )
 class Link
   include DataMapper::Resource
   property :id, Serial
